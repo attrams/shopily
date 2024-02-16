7d401cb8a451ca335f158f88e189690ff00ac5ff
attrams <48452451+Attrams@users.noreply.github.com>
2024-02-16 18:46:19 +0000

    redirect user payment view when order is placed

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .models import OrderItem
 from .forms import OrderCreateForm
@@ -30,13 +31,11 @@ def order_create(request):
             # send mail asynchronously
             order_created.delay(order.id)
 
-            return render(
-                request=request,
-                template_name='orders/order/created.html',
-                context={
-                    'order': order,
-                    'section': 'shop'
-                })
+            # set the order in the session
+            request.session['order_id'] = order.id
+
+            # redirect for payment
+            return redirect(reverse('payment:process'))
 
     else:
         form = OrderCreateForm()
