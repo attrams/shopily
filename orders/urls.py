ea6b6590bee4e34965ffa9ebae2c8b1e2c89dafc
attrams <48452451+Attrams@users.noreply.github.com>
2024-02-18 19:27:45 +0000

    add order detail view for admin

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,9 @@ app_name = 'orders'
 
 urlpatterns = [
     path(route='create/', view=views.order_create, name='order_create'),
+    path(
+        route='admin/order/<int:order_id>/',
+        view=views.admin_order_detail,
+        name='admin_order_detail'
+    )
 ]
