b38c4b9ca5712dcc736647a7f6c269aac84feb76
attrams <48452451+Attrams@users.noreply.github.com>
2024-02-22 17:33:56 +0000

    add get_absolute_url()

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -45,3 +45,6 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
