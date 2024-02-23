f75d5c92265e9503d7c09d69b63231aa94f8fdf3
attrams <48452451+Attrams@users.noreply.github.com>
2024-02-23 09:15:48 +0000

    implement tag navigation feature

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,11 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path(
+        route='tag/<slug:tag_slug>/',
+        view=views.post_list,
+        name='post_list_by_tag'
+    ),
+    path(
         route='<int:year>/<int:month>/<int:day>/<slug:post>/',
         view=views.post_detail,
         name='post_detail'
--- blog/views.py
+++ blog/views.py
@@ -3,26 +3,37 @@ from django.core.paginator import Paginator
 
 from .models import Post
 
+from taggit.models import Tag
+
 # Create your views here.
 
 
-def post_list(request):
-    posts = Post.published.all()
-    recent_three_posts = posts[:3]
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    recent_three_posts = post_list[:3]
+    tags = Tag.objects.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
     # pagination with 4 blog post per page
-    paginator = Paginator(object_list=posts, per_page=4)
+    paginator = Paginator(object_list=post_list, per_page=4)
     page_number = request.GET.get('page', 1)
-    post_list = paginator.page(number=page_number)
+    posts = paginator.page(number=page_number)
 
     return render(
         request,
         'blog/post/list.html',
         {
-            'posts': post_list,
+            # this posts is the main list of post which contains pagination
+            'posts': posts,
+            'tags': tags,
             'section': 'blog',
-            'posts_count': len(posts),
-            'recent_three_posts': recent_three_posts
+            'posts_count': len(post_list),
+            'recent_three_posts': recent_three_posts,
+            'tag': tag
         }
     )
 
